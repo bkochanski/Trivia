663335ab75a0f79e82dbcf6a08527ec4c8fea79c
bkochanski <bkochanski@zie.pg.gda.pl>
2021-01-24 18:35:10 +0100

    Update husband_wife_correlation.R

--- husband_wife_correlation.R
+++ husband_wife_correlation.R
@@ -72,3 +72,46 @@ plot(jitter(e$husband_age,3), jitter(e$wife_age,3), col=rgb(0,0,0,0.2), pch=16, 
 
 par(old.par)
 
+
+
+
+#THE SAME SCALE ON X AND Y
+
+old.par<-par(mar=rep(4,4), mfrow=c(2,2))
+
+#### INCOME
+
+plot(jitter(e2$husband_income,3), jitter(e2$wife_income,3), col=rgb(0,0,0,0.2), pch=16, log="xy", xlab="dochód męża", ylab="dochód żony", xlim=c(150,30000), ylim=c(150,30000), main=paste("DOCHÓD. Wsp. korelacji =", round(cor_income,3)))
+abline(0,1,lty=3, col="grey")
+
+
+#### HEIGHT
+
+plot(jitter(e$husband_height,3), jitter(e$wife_height,3), col=rgb(0,0,0,0.2), pch=16, xlab="wzrost męża", ylab="wzrost żony", main=paste("WZROST. Wsp. korelacji =", round(cor_height,3)), xlim=c(140,205), ylim=c(140,205))
+abline(0,1,lty=3, col="grey")
+
+#### WEIGHT
+
+plot(jitter(e$husband_weight,3), jitter(e$wife_weight,3), col=rgb(0,0,0,0.2), pch=16, xlab="masa ciała męża", ylab="masa ciała żony", main=paste("MASA CIAŁA. Wsp. korelacji =", round(cor_weight,3)), xlim=c(35,165), ylim=c(35,165))
+abline(0,1,lty=3, col="grey")
+
+
+#### AGE
+
+plot(jitter(e$husband_age,3), jitter(e$wife_age,3), col=rgb(0,0,0,0.2), pch=16, xlab="wiek męża", ylab="wiek żony", main=paste("WIEK. Wsp. korelacji =", round(cor_age,3)), xlim=c(18,100), ylim=c(18,100))
+abline(0,1,lty=3, col="grey")
+par(old.par)
+
+
+
+e$wife_BMI<-e$wife_weight/(e$wife_height^2/10000)
+e$husband_BMI<-e$husband_weight/(e$husband_height^2/10000)
+
+# correlation without survey weights
+cor(e$husband_BMI, e$wife_BMI)
+
+# correlation with survey weights
+cor_bmi<-cov.wt(data.frame(e$husband_BMI, e$wife_BMI), wt=e$avg_survey_weight, cor=TRUE)$cor[2,1]
+
+plot(jitter(e$husband_BMI,3), jitter(e$wife_BMI,3), col=rgb(0,0,0,0.2), pch=16, xlab="BMI męża", ylab="BMI żony", main=paste("BMI. Wsp. korelacji =", round(cor_bmi,3)), xlim=c(14,55), ylim=c(14,55))
+abline(0,1,lty=3, col="grey")
