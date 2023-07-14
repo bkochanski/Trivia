7b22e6004ff43d9298b6986cf280189ad1bb7e0a
bkochanski <bkochanski@zie.pg.edu.pl>
2023-07-14 18:24:16 +0200

    Create migracja_kobiet_do_miast.R

---
+++ migracja_kobiet_do_miast.R
@@ -0,0 +1,47 @@
+library(bdl)
+
+#60559 - gęstość zaludnienia (ludność na km3)
+gestosc<-get_data_by_variable("60559", unitLevel = "6", year = 2022)
+# 72301 - mężczyźni 0-4 
+m04<-get_data_by_variable("72301", unitLevel = "6", year = 2022)
+f04<-get_data_by_variable("72296", unitLevel = "6", year = 2022)
+m59<-get_data_by_variable("72302", unitLevel = "6", year = 2022)
+f59<-get_data_by_variable("72297", unitLevel = "6", year = 2022)
+# 
+m2529<-get_data_by_variable("47736", unitLevel = "6", year = 2022)
+f2529<-get_data_by_variable("47696", unitLevel = "6", year = 2022)
+m3034<-get_data_by_variable("47724", unitLevel = "6", year = 2022)
+f3034<-get_data_by_variable("47695", unitLevel = "6", year = 2022)
+
+library(dplyr)
+library(ggplot2)
+gestosc %>%
+  select(c(id, name, gestosc=val)) %>%
+  merge(select(m04, c(id, m04=val)))%>%
+  merge(select(f04, c(id, f04=val)))%>%
+  merge(select(m59, c(id, m59=val)))%>%
+  merge(select(f59, c(id, f59=val)))%>%
+  merge(select(m2529, c(id, m2529=val)))%>%
+  merge(select(f2529, c(id, f2529=val)))%>%
+  merge(select(m3034, c(id, m3034=val)))%>%
+  merge(select(f3034, c(id, f3034=val)))%>%
+  mutate(m09=m04+m59, f09=f04+f59, m2534=m2529+m3034, f2534=f2529+f3034)%>%
+  mutate(sexratio09=m09/f09, sexratio2534=m2534/f2534)%>%
+#  filter(sexratio2534>8)%>%head()
+  filter(substring(id,12,12)<"4")%>%
+  select(id, name, gestosc, sexratio09, sexratio2534) -> df
+  ggplot(df) + 
+  scale_x_continuous(trans='log10') +
+  geom_point(aes(x=gestosc, y=sexratio2534), col='red', alpha=.2) + 
+  stat_smooth(aes(x=gestosc, y=sexratio2534), method='lm', se=FALSE, alpha=.2, col='red') + 
+  geom_point(aes(x=gestosc, y=sexratio09), col='blue', alpha=.2) + 
+  stat_smooth(aes(x=gestosc, y=sexratio09), method='lm', se=FALSE, alpha=.2, col='blue') +
+  xlab("gęstość zaludnienia w osobach na km^2") +
+  ylab("stosunek liczby mężczyzn do liczby kobiet") 
+
+    cor(df$gestosc, df$sexratio09)  
+  cor(df$gestosc, df$sexratio2534)  
+  
+
+
+
