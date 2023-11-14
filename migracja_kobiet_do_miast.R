6da257fd60b339a3572ecf95f856ba96e1115ee2
bkochanski <67311101+bkochanski@users.noreply.github.com>
2023-11-14 08:54:19 +0100

    migracje

--- migracja_kobiet_do_miast.R
+++ migracja_kobiet_do_miast.R
@@ -1,17 +1,29 @@
 library(bdl)
 
 #60559 - gęstość zaludnienia (ludność na km3)
-gestosc<-get_data_by_variable("60559", unitLevel = "6", year = 2022)
+lvl<-"4"
+gestosc<-get_data_by_variable("60559", unitLevel = lvl, year = 2022)
 # 72301 - mężczyźni 0-4 
-m04<-get_data_by_variable("72301", unitLevel = "6", year = 2022)
-f04<-get_data_by_variable("72296", unitLevel = "6", year = 2022)
-m59<-get_data_by_variable("72302", unitLevel = "6", year = 2022)
-f59<-get_data_by_variable("72297", unitLevel = "6", year = 2022)
+m04<-get_data_by_variable("72301", unitLevel = lvl, year = 2022)
+f04<-get_data_by_variable("72296", unitLevel = lvl, year = 2022)
+m59<-get_data_by_variable("72302", unitLevel = lvl, year = 2022)
+f59<-get_data_by_variable("72297", unitLevel = lvl, year = 2022)
 # 
-m2529<-get_data_by_variable("47736", unitLevel = "6", year = 2022)
-f2529<-get_data_by_variable("47696", unitLevel = "6", year = 2022)
-m3034<-get_data_by_variable("47724", unitLevel = "6", year = 2022)
-f3034<-get_data_by_variable("47695", unitLevel = "6", year = 2022)
+m2529<-get_data_by_variable("47736", unitLevel = lvl, year = 2022)
+f2529<-get_data_by_variable("47696", unitLevel = lvl, year = 2022)
+m3034<-get_data_by_variable("47724", unitLevel = lvl, year = 2022)
+f3034<-get_data_by_variable("47695", unitLevel = lvl, year = 2022)
+
+# saveRDS(gestosc, 'gestosc.rds')
+# saveRDS(m04, 'm04.rds')
+# saveRDS(f04, 'f04.rds')
+# saveRDS(m59, 'm59.rds')
+# saveRDS(f59, 'f59.rds')
+# saveRDS(m2529, 'm2529.rds')
+# saveRDS(f2529, 'f2529.rds')
+# saveRDS(m3034, 'm3034.rds')
+# saveRDS(f3034, 'f3034.rds')
+
 
 library(dplyr)
 library(ggplot2)
@@ -58,12 +70,28 @@ migracje%>%
   stat_smooth(aes(x=gestosc, y=sexratio2534), method='lm', se=FALSE, alpha=.2, col='red') +
   geom_point(aes(x=gestosc, y=sexratio09), col='blue', alpha=.2) +
   stat_smooth(aes(x=gestosc, y=sexratio09), method='lm', se=FALSE, alpha=.2, col='blue') +
-  xlab("gęstość zaludnienia w osobach na km^2") +
+  xlab("gęstość zaludnienia w osobach na km") +
   ylab("stosunek liczby mężczyzn do liczby kobiet")
 
   cor(df$gestosc, df$sexratio09)  
   cor(df$gestosc, df$sexratio2534)  
   
+  cor(log(df$gestosc), df$sexratio2534)  
+  cor(rank(df$gestosc), rank(df$sexratio2534))
+  
+  plot(log(df$gestosc), df$sexratio2534)  
+  
+  plot(rank(df$gestosc), rank(df$sexratio2534), pch=16, col=rgb(0,0,0,.2))
+  
+  df %>% 
+    ggplot(aes(x=gestosc, y=sexratio2534, label=name)) + 
+  #  scale_x_continuous(trans='log10') + 
+    geom_point(alpha=1, col='red') + 
+#    stat_smooth(method='lm', se=FALSE) +
+    xlab("gęstość zaludnienia (liczba ludności przypadająca na km²)") +
+    ylab("stosunek liczby mężczyzn do liczby kobiet \nw grupie wiekowej") -> Gtemp
+  
+Gtemp
 
-
-
+ggplotly(Gtemp)
+  # https://onlinelibrary.wiley.com/doi/10.1111/padr.12577
